ff5f440c81c810b053437bd6ac0ee913ea074025
NahomGulte <100404353+NahomGulte@users.noreply.github.com>
2025-07-10 14:28:57 +0300

    Update settings.py

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'django_filters',
-    'crm.apps.CrmConfig'
+    'crm.apps.CrmConfig',
+    'django_crontab'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     'SCHEMA': 'alx_backend_graphql_crm.schema.schema',
 }
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
